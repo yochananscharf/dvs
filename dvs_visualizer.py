282aad8001ac6d6e61f16dd36e38bce3c383089b
Yochanan Scharf <yochanan@protrack.co.il>
2024-05-02 13:51:22 +0300

    delta-t delta-step

--- dvs_visualizer.py
+++ dvs_visualizer.py
@@ -4,11 +4,21 @@ import dv_processing as dv
 from PIL import Image, ImageDraw 
 from datetime import timedelta
 import numpy as np
+from streamlit import session_state as ss
 import streamlit as st
+from pathlib import Path
+from time import sleep
 
-from skimage.transform import hough_circle
+
+
+from eyeTracker import EyeTracking, file_path_a
+
+st.title('Event Based Camera - Eye Tracking')
+sidebar = st.sidebar
 col1, col2 = st.columns(2)
+
 image_area = col1.empty()
+image_area1 = col1.empty()
 img_plot_area =[]
 for i in range(4):
     img_plot_area.append(col2.empty())
@@ -16,140 +26,25 @@ for i in range(4):
 # event_area_2 = col2.empty()
 # event_area_3 = col2.empty()
 # event_area_2 = col2.empty()
-# Open the camera, just use first detected DAVIS camera
-#camera = dv.io.CameraCapture("", dv.io.CameraCapture.CameraType.DAVIS)
-# Open a file
-file_path_a  = 'data/dvSave-2024_01_09_18_02_30.aedat4'
-file_path_b  = 'data/dvSave-2024_01_09_18_02_32.aedat4'
 
 
 
-camera = dv.io.MonoCameraRecording(file_path_a)
-
-# Initialize a multi-stream slicer
-slicer = dv.EventMultiStreamSlicer("events")
-
-# Add a frame stream to the slicer
-slicer.addFrameStream("frames")
-
-# Initialize a visualizer for the overlay
-resolution = camera.getEventResolution()
-
-
-
-#crop_box_resize = (crop_box[2], crop_box[3])
-#filter = dv.EventRegionFilter(crop_box)
-
-
-visualizer = dv.visualization.EventVisualizer(camera.getEventResolution(), dv.visualization.colors.white(),
-                                              dv.visualization.colors.green(), dv.visualization.colors.red())
-
-visualizer_true = dv.visualization.EventVisualizer(camera.getEventResolution(), dv.visualization.colors.black(),
-                                              dv.visualization.colors.green(), dv.visualization.colors.red())
-visualizer_false = dv.visualization.EventVisualizer(camera.getEventResolution(), dv.visualization.colors.black(),
-                                              dv.visualization.colors.green(), dv.visualization.colors.red())
-
-
-class EyeTracking():
-    def __init__(self) -> None:
-        self.visualize_image_size = resolution
-        offset = int(resolution[0]/12)
-        self.crop_box = (int(resolution[0]/2), offset, resolution[0], int(resolution[1]/2)+offset)
-        self.pupil_width = 100 # in pixels
-        self.pupil_start_x = 180
-        self.pupil_start_y = 45
-        self.draw_blank =  Image.new('RGBA', self.visualize_image_size, (255, 255, 255, 0))#for opacity 
-        self.roi_pupil = (self.pupil_start_x, self.pupil_start_y,  self.pupil_start_x+int(1.25*self.pupil_width),self.pupil_start_y+int(1.25*self.pupil_width))
-        x = int(self.crop_box[0]+self.pupil_start_x/3)
-        y = int(self.crop_box[1]+self.pupil_start_y/4)
-        w = int(self.pupil_width)
-        h = int(self.pupil_width)
-        self.roi = (x, 
-                    y, 
-                    x+w, 
-                    y+h)
-        self.roi_left = (x, y, x+w//2, y+h)
-        self.roi_right = (x+w//2, y, x+w, y+h)
-        self.filter_pos = None
-        self.filter_neg = None
-        self.filter_region_left =    None
-        self.filter_region_right =   None
-        self.filter_chain_left_pos = None
-        self.filter_chain_right_pos =None
-        self.filter_chain_left_neg = None
-        self.filter_chain_right_neg =None
-        self.filtered_counts = []
-        self.create_filters()
-        # self.filters_dict = {
-        #     'left-pos':self.filter_region_left_pos, 
-        #     'right-pos':self.filter_region_right_pos,
-        #     'left-neg':self.filter_region_left_neg ,
-        #     'right-neg':self.filter_region_right_neg,
-        # }
-        self.count = 0
-        pass
-
-    def create_filters(self):
-        self.filter_pos = dv.EventPolarityFilter(True)
-        self.filter_neg = dv.EventPolarityFilter(False)
-        filter_region_left = dv.EventRegionFilter(self.roi_left)
-        filter_region_right = dv.EventRegionFilter(self.roi_right)
-        filter_chain_left_pos = dv.EventFilterChain()
-        filter_chain_left_pos.addFilter(self.filter_pos)
-        filter_chain_left_pos.addFilter(filter_region_left)
-        filter_chain_right_pos = dv.EventFilterChain()
-        filter_chain_right_pos.addFilter(self.filter_pos)
-        filter_chain_right_pos.addFilter(filter_region_right)
-        filter_chain_left_neg = dv.EventFilterChain()
-        filter_chain_left_neg.addFilter(self.filter_neg)
-        filter_chain_left_neg.addFilter(filter_region_left)
-        filter_chain_right_neg = dv.EventFilterChain()
-        filter_chain_right_neg.addFilter(self.filter_neg)
-        filter_chain_right_neg.addFilter(filter_region_right)
-        self.filters_list = [filter_chain_left_pos, 
-                             filter_chain_right_pos,
-                             filter_chain_left_neg,
-                             filter_chain_right_neg]
-
-    def draw_image(self, image_raw, roi):
-        blank = self.draw_blank.copy()
-        draw = ImageDraw.Draw(blank)   
-        draw.rectangle(roi, width=3, outline = (255, 0, 0, 80)) 
-        out = Image.alpha_composite(image_raw.convert('RGBA'), blank)
-        return out
-    
-
-    def events_stats(self, events):
-        num_events = events.size()
-        return num_events
-
-    def filter_events_regions(self, events):
-        filtered_events_list = []
-        for filter_chain in self.filters_list:
-            filter_chain.accept(events) 
-            filtered_events = filter_chain.generateEvents()
-            filtered_events_list.append(filtered_events)
-        #filtered_events_list = [left_pos, right_pos, left_neg, right_neg]
-        #filtered_list = [pos_left, pos_right, neg_left, neg_right]
-        self.filtered_counts = [self.events_stats(evnt) for evnt in filtered_events_list]
-        print(self.filtered_counts)
-        return filtered_events_list
-
-eye_tracker = EyeTracking()
 # Callback method for time based slicing
 def display_preview(data):
-
-    # Retrieve frame data using the named method and stream name
+    sleep(0.2)
+    #if st.sidebar.button('next-frame',count):
+        # Retrieve frame data using the named method and stream name
     frames = data.getFrames("frames")
 
     # Retrieve event data
     events = data.getEvents("events")
+    
     #filter for positive polarity events
-    eye_tracker.filter_pos.accept(events)
-    filtered_pos = eye_tracker.filter_pos.generateEvents()
-    eye_tracker.filter_neg.accept(events)
-    filtered_neg = eye_tracker.filter_neg.generateEvents()
-    filtered_list= eye_tracker.filter_events_regions(events)
+    ss.eye_tracker.filter_pos.accept(events)
+    filtered_pos = ss.eye_tracker.filter_pos.generateEvents()
+    ss.eye_tracker.filter_neg.accept(events)
+    filtered_neg = ss.eye_tracker.filter_neg.generateEvents()
+    filtered_list= ss.eye_tracker.filter_events_regions(events)
     #filter.accept(events)
     #filtered = filter.generateEvents()
     # Retrieve and color convert the latest frame of retrieved frames
@@ -166,42 +61,79 @@ def display_preview(data):
         return
 
     # Generate a preview and show the final image
-    preview_image = Image.fromarray(visualizer.generateImage(events, latest_image))
-    preview_image_cropped = preview_image.crop(eye_tracker.crop_box)
-    preview_image_cropped = preview_image_cropped.resize(resolution) # back to size of original image
-    image_draw = eye_tracker.draw_image(preview_image_cropped, eye_tracker.roi_pupil)
+    preview_image = Image.fromarray(ss.eye_tracker.visualizer.generateImage(events, latest_image))
+    preview_image_cropped = preview_image.crop(ss.eye_tracker.crop_box)
+    preview_image_cropped = preview_image_cropped.resize(ss.eye_tracker.resolution) # back to size of original image
+    image_draw = ss.eye_tracker.draw_image(preview_image_cropped, ss.eye_tracker.roi_pupil)
     image_area.image(image_draw)
-
-    image_pos = Image.fromarray(visualizer_true.generateImage(filtered_pos))
-    image_pos_cropped = image_pos.crop(eye_tracker.roi_right)
-    #image_pos = eye_tracker.draw_image(image_pos)
-    image_neg = Image.fromarray(visualizer_false.generateImage(filtered_neg))
-    image_neg_cropped = image_neg.crop(eye_tracker.roi_left)
-    for j, fltrd in enumerate(filtered_list):
-        events_image = Image.fromarray(visualizer_false.generateImage(fltrd))
-        if (j%2)==0:
-            image_draw = eye_tracker.draw_image(events_image, eye_tracker.roi_left)
-        else:
-            image_draw = eye_tracker.draw_image(events_image, eye_tracker.roi_right)
-        img_plot_area[j].image(image_draw, width=200)
+    ss.eye_tracker.filter_region.accept(events)
+    events_area = ss.eye_tracker.filter_region.generateEvents()
     
-    print(eye_tracker.count)
-    eye_tracker.count +=1
-    
-    # if count >= 277:
-    #     check = True
-    #event_area_1.image(image_pos_cropped)
-    #event_area_2.image(image_neg_cropped)
 
-    num_pos = eye_tracker.events_stats(filtered_pos)
-    num_neg = eye_tracker.events_stats(filtered_neg)
-    if events.size() > 300:
-        movement_direction  = ''
-        if eye_tracker.filtered_counts[2] >  1.2*eye_tracker.filtered_counts[3]:
-            movement_direction = 'left'
-        else:
-            movement_direction = 'right'
-        col1.write('moving {} '.format(movement_direction))
+    image_array = ss.eye_tracker.visualizer_gray.generateImage(events_area)
+    image_events = Image.fromarray(image_array)
+
+    #plot events
+    preview_image_cropped = image_events.crop(ss.eye_tracker.crop_box)
+    preview_image_cropped = preview_image_cropped.resize(ss.eye_tracker.resolution)
+    image_area1.image(preview_image_cropped)
+
+    # plot polarities separate
+    image_blue = Image.fromarray(image_array[:,:,2]).crop(ss.eye_tracker.crop_box)
+    image_red = Image.fromarray(image_array[:,:,1]).crop(ss.eye_tracker.crop_box)
+
+    img_plot_area[0].image(image_blue, width=346)
+    img_plot_area[1].image(image_red, width=346)
+    #image = img_as_ubyte(image_events)
+    #edges = canny(image, sigma=5)
+    #c = ndi.center_of_mass(image)
+    #r = 8
+    #draw_blank =  Image.new('RGBA', ss.eye_tracker.resolution, (255, 255, 255, 0))
+    #draw = ImageDraw.Draw(draw_blank)
+    #draw.ellipse((cx-r, cy-r, cx+r, cy+r), fill=(255,0,0))
+    #out = Image.alpha_composite(image_events.convert('RGBA'), draw_blank)
+
+    # if accums[0] >0.05:
+    #     draw_blank =  Image.new('RGBA', resolution, (255, 255, 255, 0))
+    #     draw = ImageDraw.Draw(draw_blank)
+    #     draw.ellipse((cx-r, cy-r, cx+r, cy+r), outline=(255,0,0))
+    #     out = Image.alpha_composite(image_events.convert('RGBA'), draw_blank)
+    #     img_plot_area[0].image(out, width=300)
+    #print(ss.eye_tracker.count)
+
+    # save events for future use
+    for evnt in events:
+        ss.eye_tracker.store.push_back(evnt)
+    if len(events_area) > 0:
+        ss.eye_tracker.save_events(events_area)
+    ss.eye_tracker.count +=1
+    if False:
+        image_pos = Image.fromarray(visualizer_true.generateImage(filtered_pos))
+        image_pos_cropped = image_pos.crop(eye_tracker.roi_right)
+        #image_pos = eye_tracker.draw_image(image_pos)
+        image_neg = Image.fromarray(visualizer_false.generateImage(filtered_neg))
+        image_neg_cropped = image_neg.crop(eye_tracker.roi_left)
+        for j, fltrd in enumerate(filtered_list):
+            events_image = Image.fromarray(visualizer_false.generateImage(fltrd))
+            if (j%2)==0:
+                image_draw = eye_tracker.draw_image(events_image, eye_tracker.roi_left)
+            else:
+                image_draw = eye_tracker.draw_image(events_image, eye_tracker.roi_right)
+            img_plot_area[j].image(image_draw, width=200)
+        
+
+
+
+
+        num_pos = eye_tracker.events_stats(filtered_pos)
+        num_neg = eye_tracker.events_stats(filtered_neg)
+        if events.size() > 300:
+            movement_direction  = ''
+            if eye_tracker.filtered_counts[2] >  1.2*eye_tracker.filtered_counts[3]:
+                movement_direction = 'left'
+            else:
+                movement_direction = 'right'
+            col1.write('moving {} '.format(movement_direction))
     #     p
     # when moving, there should be a large but similar amount of negative and positive events
     # as the darker pupil moves right: negative events to the right, positive events to the left and vise versa
@@ -210,18 +142,44 @@ def display_preview(data):
     # If escape button is pressed (code 27 is escape key), exit the program cleanly
     #if cv.waitKey(2) == 27:
     #    exit(0)
-
-
-# Register a job to be performed every 33 milliseconds
-slicer.doEveryTimeInterval(timedelta(milliseconds=50), display_preview)
-
-# Continue the loop while both cameras are connected
-while camera.isRunning():
     
-    events = camera.getNextEventBatch()
-    if events is not None:
-        slicer.accept("events", events)
+if __name__ == '__main__':
+    if 'count' in ss:
+        #clicked = sidebar.button('save events', on_click=ss.eye_tracker.save_events(), key=ss.count)
+        #if clicked:
+            # events = ss.eye_tracker.camera.getNextEventBatch()
+            # frame = ss.eye_tracker.camera.getNextFrame()
+        while  ss.eye_tracker.camera.isRunning():
+            ss.count += 1
+            
+            events = ss.eye_tracker.camera.getNextEventBatch()
+            if events is not None:
+                ss.eye_tracker.slicer.accept("events", events)
 
-    frame = camera.getNextFrame()
-    if frame is not None:
-        slicer.accept("frames", [frame])
+            frame = ss.eye_tracker.camera.getNextFrame()
+            if frame is not None:
+                ss.eye_tracker.slicer.accept("frames", [frame])
+            #clicked = False
+    else:
+        
+        ss.dvs_file = file_path_a#sidebar.file_uploader('choose file').name
+        ss.delta_t = sidebar.slider('Set Time-Delta', min_value=10, max_value=100, value=30)
+        ss.step_t = sidebar.slider('Set Step-Delta', min_value=10, max_value=50, value=10)
+        if ss.dvs_file is not None:
+            ss.count = 0
+            data_dir = ss.dvs_file.split('.')[0]
+            running_dir = Path.cwd()
+            data_path = running_dir/data_dir
+            Path.mkdir(data_path,exist_ok=True)
+            ss.eye_tracker = EyeTracking(ss.dvs_file)
+            ss.eye_tracker.delta_t = ss.delta_t
+            ss.eye_tracker.step_t = ss.step_t
+            ss.eye_tracker.data_path = data_path
+            ss.eye_tracker.slicer.doEveryTimeInterval(timedelta(milliseconds=ss.step_t), display_preview)
+            
+            #ss.eye_tracker.run_events()
+            st.rerun()
+
+                    # Register a job to be performed every 33 milliseconds
+    
+    
---
+++ eyeTracker.py
@@ -0,0 +1,181 @@
+import dv_processing as dv
+from PIL import Image, ImageDraw 
+from datetime import timedelta
+import numpy as np
+from streamlit import session_state as ss
+import streamlit as st
+
+from time import sleep
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.util import img_as_ubyte
+import scipy.ndimage as ndi
+import pandas as pd
+
+
+file_path_a  = 'data/dvSave-2024_01_09_18_02_30.aedat4'
+file_path_b  = 'data/dvSave-2024_01_09_18_02_32.aedat4'
+
+class EyeTracking():
+    def __init__(self, file_name= file_path_a) -> None:
+       
+        self.camera         =None  
+        self.slicer=None
+        self.resolution=None
+        self.visualizer=None
+        self.visualizer_true=None
+        self.visualizer_fals=None
+        self.visualizer_gray=None
+        self.data_path = ''
+        self.prev_time = -1
+        self.create_visualizers()
+        self.visualize_image_size = self.resolution
+        offset = int(self.resolution[0]/12)
+        self.crop_box = (int(self.resolution[0]/2), offset, self.resolution[0], int(self.resolution[1]/2)+offset)
+        self.pupil_width = 100 # in pixels
+        self.pupil_start_x = 180
+        self.pupil_start_y = 45
+        self.draw_blank =  Image.new('RGBA', self.visualize_image_size, (255, 255, 255, 0))#for opacity 
+        self.roi_pupil = (self.pupil_start_x, self.pupil_start_y,  self.pupil_start_x+int(1.25*self.pupil_width),self.pupil_start_y+int(1.25*self.pupil_width))
+        x = int(self.crop_box[0]+self.pupil_start_x/2)
+        y = int(self.crop_box[1]+self.pupil_start_y/4)
+        w = int(self.pupil_width)
+        h = int(self.pupil_width)
+        self.roi = (x, 
+                    y, 
+                    x+w, 
+                    y+h)
+        self.roi_left = (x, y, x+w//2, y+h)
+        self.roi_right = (x+w//2, y, x+w, y+h)
+        self.filter_pos = None
+        self.filter_neg = None
+        self.filter_region = None
+        self.filter_region_left =    None
+        self.filter_region_right =   None
+        self.filter_chain_left_pos = None
+        self.filter_chain_right_pos =None
+        self.filter_chain_left_neg = None
+        self.filter_chain_right_neg =None
+        self.filtered_counts = []
+        self.hough_radii = np.arange(85, 120, 5)
+        self.create_filters()
+        
+        # self.filters_dict = {
+        #     'left-pos':self.filter_region_left_pos, 
+        #     'right-pos':self.filter_region_right_pos,
+        #     'left-neg':self.filter_region_left_neg ,
+        #     'right-neg':self.filter_region_right_neg,
+        # }
+        self.count = 0
+        self.store = dv.EventStore()
+        self.step_t = -1
+        self.delta_t = 50
+        pass
+
+    def create_visualizers(self):
+
+        self.camera = dv.io.MonoCameraRecording(file_path_a)
+
+        # Initialize a multi-stream slicer
+        self.slicer = dv.EventMultiStreamSlicer("events")
+
+        # Add a frame stream to the slicer
+        self.slicer.addFrameStream("frames")
+
+        # Initialize a visualizer for the overlay
+        self.resolution = self.camera.getEventResolution()
+
+
+        self.visualizer = dv.visualization.EventVisualizer(self.camera.getEventResolution(), dv.visualization.colors.white(),
+                                                    dv.visualization.colors.green(), dv.visualization.colors.red())
+
+        self.visualizer_true = dv.visualization.EventVisualizer(self.camera.getEventResolution(), dv.visualization.colors.black(),
+                                                    dv.visualization.colors.green(), dv.visualization.colors.red())
+        self.visualizer_false = dv.visualization.EventVisualizer(self.camera.getEventResolution(), dv.visualization.colors.black(),
+                                                    dv.visualization.colors.green(), dv.visualization.colors.red())
+
+        self.visualizer_gray = dv.visualization.EventVisualizer(self.camera.getEventResolution(), dv.visualization.colors.black(),
+                                                    dv.visualization.colors.green(), dv.visualization.colors.red())#, dv.visualization.colors.white())
+
+    def create_filters(self):
+        self.filter_pos = dv.EventPolarityFilter(True)
+        self.filter_neg = dv.EventPolarityFilter(False)
+        self.filter_region = dv.EventRegionFilter(self.roi)
+        filter_region_left = dv.EventRegionFilter(self.roi_left)
+        filter_region_right = dv.EventRegionFilter(self.roi_right)
+        filter_chain_left_pos = dv.EventFilterChain()
+        filter_chain_left_pos.addFilter(self.filter_pos)
+        filter_chain_left_pos.addFilter(filter_region_left)
+        filter_chain_right_pos = dv.EventFilterChain()
+        filter_chain_right_pos.addFilter(self.filter_pos)
+        filter_chain_right_pos.addFilter(filter_region_right)
+        filter_chain_left_neg = dv.EventFilterChain()
+        filter_chain_left_neg.addFilter(self.filter_neg)
+        filter_chain_left_neg.addFilter(filter_region_left)
+        filter_chain_right_neg = dv.EventFilterChain()
+        filter_chain_right_neg.addFilter(self.filter_neg)
+        filter_chain_right_neg.addFilter(filter_region_right)
+        self.filters_list = [filter_chain_left_pos, 
+                             filter_chain_right_pos,
+                             filter_chain_left_neg,
+                             filter_chain_right_neg]
+
+    def draw_image(self, image_raw, roi):
+        blank = self.draw_blank.copy()
+        draw = ImageDraw.Draw(blank)   
+        draw.rectangle(roi, width=3, outline = (255, 0, 0, 80)) 
+        out = Image.alpha_composite(image_raw.convert('RGBA'), blank)
+        return out
+    
+
+    def events_stats(self, events):
+        num_events = events.size()
+        return num_events
+
+    def filter_events_regions(self, events):
+        filtered_events_list = []
+        for filter_chain in self.filters_list:
+            filter_chain.accept(events) 
+            filtered_events = filter_chain.generateEvents()
+            filtered_events_list.append(filtered_events)
+        #filtered_events_list = [left_pos, right_pos, left_neg, right_neg]
+        #filtered_list = [pos_left, pos_right, neg_left, neg_right]
+        self.filtered_counts = [self.events_stats(evnt) for evnt in filtered_events_list]
+        #print(self.filtered_counts)
+        return filtered_events_list
+
+
+    def detect_circle(self, edges):
+        
+        hough_res = hough_circle(edges, self.hough_radii)
+        accums, cx, cy, radii = hough_circle_peaks(hough_res, self.hough_radii,
+                                           total_num_peaks=1)
+        print(accums, cx, cy)
+        return accums, cx, cy
+    
+    def events_to_csv(self, events):
+        highest_t = events.getHighestTime()
+        events_list =[ {'x':event.x(),'y': event.y(), 'polarity': event.polarity()} for event in events ]
+        df = pd.DataFrame(events_list)
+        path_str = '{}_{}_{}_{}.csv'.format(self.count, str(highest_t), self.delta_t, self.step_t)
+        df_path = self.data_path/path_str
+        df.to_csv(df_path, index=False)
+
+    def save_events(self, events):#, events, image):
+        
+        #print(self.count)
+        highest_t = events.getHighestTime()
+        lowest_t = events.getLowestTime()
+
+
+        if self.step_t < self.delta_t:
+            step_start = highest_t - self.delta_t*1000
+            step_stop = highest_t+1
+            #delta_start = lowest_t
+            #while step_start > delta_start:
+            delta_slice = self.store.sliceTime(step_start, step_stop)
+            #step_start -= self.step_t*1000
+            self.events_to_csv(delta_slice)
+        else:
+
+            self.events_to_csv(events)
+        #image.convert('RGB').save('{}.jpg'.format(self.count))
